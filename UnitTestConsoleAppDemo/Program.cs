afba8c730766be2b26447786f4d0e81159d6e26d
Martin Kierkegaard <martinkierkegaard@yahoo.com>
2026-09-07 11:02:48 +0200

    tilføjet en new i program

--- UnitTestConsoleAppDemo/Program.cs
+++ UnitTestConsoleAppDemo/Program.cs
@@ -1,2 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using UnitTestConsoleAppDemo;
+
 Console.WriteLine("Hello, World!");
+
+Console.WriteLine(new SkatteBeregner().BeregnSkat(10000, 1));
