abe63e80797f943b7127db5c9da715474d7871e3
Martin Kierkegaard <mark@zealand.dk>
2026-09-07 10:56:14 +0200

    Initial commit

---
+++ UnitTestConsoleAppDemo/SkatteBeregner.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestConsoleAppDemo
+{
+    public class SkatteBeregner
+    {
+        /// <summary>
+        /// En metode til at beregne skat, lav eller høj skat
+        /// Hvis jobKategorien er pensionist eller studerende betales kun 5%
+        /// </summary>
+        /// <param name="indkomst">hvilken indkomst har personen</param>
+        /// <param name="jobKategori 1: studerende, 2: pensionist, 3:underviser på Zealand">hvilken kategori er personen</param>
+        /// <returns>skat der skal betales</returns>
+        public int BeregnSkat(int indkomst,int jobKategori)
+        {
+
+
+            int specialSkat = 5;
+            int lavSkat = 10;
+            int HøjSkat = 20;
+
+
+
+            //no tax
+            if (indkomst <= 0)
+            {
+                return 0;
+            }
+
+            if (jobKategori == 1 || jobKategori == 2)
+                return indkomst * specialSkat / 100;
+
+            //low tax 
+            if (indkomst <= 20000)
+                return indkomst * lavSkat / 100;
+            //high tax
+            else
+                return indkomst * HøjSkat / 100;
+
+        }
+
+    }
+}
---
+++ UnitTestConsoleAppDemoTests1/SkatteBeregnerTests.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UnitTestConsoleAppDemo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestConsoleAppDemo.Tests
+{
+    [TestClass()]
+    public class SkatteBeregnerTests
+    {
+        [TestMethod()]
+        public void BeregnSkatHvisIndkomstEr0ellerMindreTest()
+        {
+            //Arrange
+            SkatteBeregner sb = new SkatteBeregner();
+
+            //Act
+            int actualValue = sb.BeregnSkat(0, 4);
+
+            //Assert
+            Assert.AreEqual(0, actualValue);
+
+        }
+
+
+        [TestMethod()]
+        public void BeregnSkatIndkomstEr20000LavSKatTest()
+        {
+            //Arrange
+            SkatteBeregner sb = new SkatteBeregner();
+
+            //Act
+            int actualValue = sb.BeregnSkat(20000, 4);
+
+            //Assert
+
+            Assert.AreEqual(2000, actualValue);
+
+        }
+
+        [DataTestMethod]
+        [DataRow(1, 0, 0)]
+        [DataRow(2, 0, 0)]
+        [DataRow(1, -1, 0)]
+        [DataRow(2, -1, 0)]
+        [DataRow(1, 10000, 500)]
+        [DataRow(2, 10000, 500)]
+        [DataRow(1, 20000, 1000)]
+        [DataRow(2, 20000, 1000)]
+        [DataRow(1, 20001, 1000)]
+        [DataRow(2, 20001, 1000)]
+        [DataRow(3, 20000, 2000)]
+        [DataRow(3, 20001, 4000)]
+
+        public void BeregnSkatPersonKategori1_2_3(int kategori, int indkomst, int expectedValue)
+        {
+            //arrange
+            SkatteBeregner sb = new SkatteBeregner();
+
+            //Act
+            int actualValue = sb.BeregnSkat(indkomst, kategori);
+
+            //Assert
+            Assert.AreEqual(expectedValue, actualValue);
+        }
+
+    }
+}
